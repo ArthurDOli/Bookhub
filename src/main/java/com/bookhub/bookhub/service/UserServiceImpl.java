3769f6aff94caae150158bb7de23fbee98699ef8
Arthur <arthur.duarte.oli06@gmail.com>
2026-01-02 20:38:55 -0300

    feat(UserServiceImpl): get users and change user role method added

--- src/main/java/com/bookhub/bookhub/service/UserServiceImpl.java
+++ src/main/java/com/bookhub/bookhub/service/UserServiceImpl.java
@@ -7,6 +7,9 @@ import com.bookhub.bookhub.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
@@ -49,6 +52,7 @@ public class UserServiceImpl implements UserService {
         return userRepository.save(user);
     }
 
+    @Override
     public void deleteUser(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User", id));
@@ -61,5 +65,39 @@ public class UserServiceImpl implements UserService {
         }
 
         userRepository.delete(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    @Override
+    public User changeUserRole(Long userId, User.Role newRole) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", userId));
+
+        if (user.getRole() == newRole) {
+            throw new IllegalArgumentException("This is already the User's role.");
+        }
+
+        user.setRole(newRole);
+
+        return userRepository.save(user);
     }
 }

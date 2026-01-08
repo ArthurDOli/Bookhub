d23a88c8597ad32275c36eef0d2d08f578cc044f
Arthur <arthur.duarte.oli06@gmail.com>
2026-01-08 13:58:50 -0300

    feat: BookUpdateRequest added

--- src/main/java/com/bookhub/bookhub/dto/book/request/BookUpdateRequest.java
+++ src/main/java/com/bookhub/bookhub/dto/book/request/BookUpdateRequest.java
@@ -1,25 +1,20 @@
 package com.bookhub.bookhub.dto.book.request;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-public class BookCreateRequest {
-    @NotBlank(message = "Title is required")
+public class BookUpdateRequest {
     @Size(min = 1, max = 200, message = "Title must be between 1 and 200 characters")
     private String title;
 
-    @NotBlank(message = "Author is required")
-    @Size(min = 1, max = 100, message = "Author must be between 1 and 100 characters")
+    @Size(min = 1, max = 200, message = "Author must be between 1 and 100 characters")
     private String author;
 
     @Min(value = 0, message = "Publication year cannot be negative")
     private Integer publicationYear;
 
     @Min(value = 1, message = "Total copies must be at least 1")
-    private Integer totalCopies = 1;
-
-    private String isbn;
+    private Integer totalCopies;
 }

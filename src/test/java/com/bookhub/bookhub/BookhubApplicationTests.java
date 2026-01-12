119b386e3ddd5d1a693f27911ebc8f2c4efa1b7c
Arthur <arthur.duarte.oli06@gmail.com>
2026-01-12 16:56:17 -0300

    feat: Docker added

--- src/test/java/com/bookhub/bookhub/BookhubApplicationTests.java
+++ src/test/java/com/bookhub/bookhub/BookhubApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bookhub.bookhub;
+package com.bookhub.bookhub;intelliJ rodando, 7 abas
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

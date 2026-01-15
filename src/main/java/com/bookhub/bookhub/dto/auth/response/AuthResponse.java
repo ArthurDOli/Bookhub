253d05dbd58e66976acf8fcdfb231440f31525b0
Arthur <arthur.duarte.oli06@gmail.com>
2026-01-15 11:12:43 -0300

    feat: AuthResponse added

---
+++ src/main/java/com/bookhub/bookhub/dto/auth/response/AuthResponse.java
@@ -0,0 +1,24 @@
+package com.bookhub.bookhub.dto.auth.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String type = "Bearer";
+    private UserInfo user;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UserInfo {
+        private Long id;
+        private String name;
+        private String email;
+        private String role;
+    }
+}
